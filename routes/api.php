55f12e055202bc6168b3e6f8540f10374c481505
King's Palace Real Estate <it.kpre@gmail.com>
2026-08-07 09:29:29 +0400

    Initial commit by Mohamed Al Hamdhy

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('user.home');
+});
+
+
+Route::get('/admin', function () {
+    return view('admin.admin');
+});
